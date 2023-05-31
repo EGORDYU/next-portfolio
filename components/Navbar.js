b2dd9cd003661504cfda08f80f85b10f6abef2f1
EGORDYU <egordyuzhev@gmail.com>
2023-05-31 16:33:18 -0600

    no nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,9 @@ import Link from "next/link"
 export default function Navbar() {
     return(
         <nav>
-            <Link href="/">Home</Link>
+            {/* <Link href="/">Home</Link>
 
-            <Link href="/about">About Me</Link>
+            <Link href="/about">About Me</Link> */}
         </nav>
     )
 }
